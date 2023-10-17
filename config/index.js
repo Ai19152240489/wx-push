70eb524bed741f94b8cedcbb409ed02732a9011d
Ai19152240489 <148177033+Ai19152240489@users.noreply.github.com>
2023-10-17 11:38:46 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -2,11 +2,9 @@ module.exports = {
   appid: "wxa7edd062bfcfc113",
   appsecret: "e098cfe2670eaf82ea30568250713f33",
   template_id: "OsGMxGbSk6u5M17iKFTSalok0T26yPzcaxv7YDfCB9o",
-  touser1: "oM_R16m_WcAr6bXpRcHpjz6R31xs",
-  touser2: "oM_R16m_WcAr6bXpRcHpjz6R31xs",
+  touser: "oM_R16m_WcAr6bXpRcHpjz6R31xs",
 
   key: "201c93c2e2b34bdeecdbf9f5265eb84e",
-  city1: "益阳",
-  city2: "~~",
+  city: "益阳",
   first_date: "2023-10-1",
 };
